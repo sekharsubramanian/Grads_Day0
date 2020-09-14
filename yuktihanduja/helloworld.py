47181bf4925467aeb7f80f53a429948ff039021b
yuktihanduja <50393281+yuktihanduja@users.noreply.github.com>
2020-09-15 02:14:47 +0530

    Create helloworld.py

---
+++ yuktihanduja/helloworld.py
@@ -0,0 +1,5 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("hello World")
+hello World
+>>> 
