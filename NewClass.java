688f4ec01520963cac129a1cf0a7c40df41368f6
unknown <shantanu.kaushik21@gmail.com>
2020-09-14 22:51:13 +0530

    first commit

---
+++ NewClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codeforces;
+import java.util.Scanner;
+
+/**
+ *
+ * @author SHANTANU KAUSHIK
+ */
+public class NewClass {
+
+    /**
+     * @param args the command line arguments
+     */
+  
+    public static void main(String[] args){
+            System.out.println("Hello world, This is shantanu");
+        }
+}
+
