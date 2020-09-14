557c374bf982dd8addf2e0191fc33f5740d1b0da
Mrunal Ahire <mrunal.ahire@gmail.com>
2020-09-14 16:13:36 +0530

    Added i-64

---
+++ i-64/a.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define x first
+#define y second
+#define endl '\n'
+#define pb push_back
+#define int long long
+#define double long double
+const double pi = 2.0 * acos(0.0);
+#define setp(x) cout<<setprecision(x);
+#define xrange(i,a,b) for(int i=a;i<b;i++)
+#define yrange(i,b,a) for(int i=b;i>=a;i--)
+const int mod1 = 1000000007, mod2 = 998244353;
+#define deb(...) pr(#__VA_ARGS__, __VA_ARGS__);
+#define usefile freopen("in.txt","r",stdin);freopen("out.txt","w",stdout);
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);setp(9);
+#define tezz int32_t main () { fast; solve(); return 0;}
+#define tezzn int32_t main (){fast;/*cin>>t*/;while(t--)solve();return 0;}
+#define debcon(v) cerr<<#v<<": [ ";for(auto e:v)cerr<<e<<", ";cerr<<"]"<<endl;
+template <typename T, typename S>ostream& operator<< (ostream& os,
+const pair<T,S>& p){os<<"("<<p.x<<","<<p.y<<")";return os;}
+template<typename T>void pr(const string& name,T t){cerr<<name<<": "<<t<<endl;}
+template<typename T,typename...Types>void pr(const string& names,T t,
+Types...rest){auto cp=names.find(',');cerr<<names.substr(0,cp)<<": "<<t<<", ";
+auto nnp=names.find_first_not_of(" \t\n",cp+1);pr(string(names,nnp),rest...);}
+//=============================================================================
+
+int n;
+
+void solve () {
+	cout << "hello world" << endl;
+}
+tezz
