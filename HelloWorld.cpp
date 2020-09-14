ea4fbc4e908af727ba4e07a12bf0bbd56aacf1a2
Varun Gujarathi <v.gujarathi777@gmail.com>
2020-09-14 16:55:19 +0530

    Created helloworld in C++

---
+++ HelloWorld.cpp
@@ -0,0 +1,16 @@
+/*
+Hello World program
+
+@author: Varun Gujarathi
+@createdOn: 14th Sept 2020
+
+Writing this C++ since its my top fav language
+*/
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World";
+    return 0;
+}
