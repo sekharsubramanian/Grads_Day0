c7806dfb7ddc1e25b264c859c43ae18c129abe7d
Sakshi-123456 <66631528+Sakshi-123456@users.noreply.github.com>
2020-09-15 07:37:55 +0530

    Add files via upload

---
+++ Sakshi-12345.java
@@ -0,0 +1,13 @@
+
+public class main_assignement0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World!!!");
+	}
+
+}
+
+/*
+Hello World!!!
+*/
