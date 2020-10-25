47944b2459306cbb6ce0c7953ec75211e5448875
padmapt <tgangba4@gmail.com>
2020-10-25 17:26:28 +0530

    Create helloworld.cpp

---
+++ padmapt/helloworld.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Padma Tashi on 25/10/20.
+//  Copyright © 2020 Padma Tashi. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello World!\n";
+    return 0;
+}
