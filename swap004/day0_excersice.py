76bb9ef162db4c6d1e5699dce4ef349486c1141a
swap004 <swapnilpat04@gmail.com>
2020-10-25 16:35:19 +0530

    Create day0_excersice.py

---
+++ swap004/day0_excersice.py
@@ -0,0 +1,9 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 03:03:55) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+>>> print("Hello World!")
+Hello World!
+>>> 
